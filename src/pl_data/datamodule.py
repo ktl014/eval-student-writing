2e2b5b8b817a0c71e13ea5dadc92cc967981eac6
ktl014 <ktl014@ucsd.edu>
2021-12-20 10:53:30 -0800

    feat: adapt datamodule to ews dataset. not tested yet

--- src/pl_data/datamodule.py
+++ src/pl_data/datamodule.py
@@ -8,6 +8,8 @@ import pytorch_lightning as pl
 import torch
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset
+from transformers import AutoTokenizer
 
 from src.common.utils import PROJECT_ROOT
 
@@ -36,11 +38,15 @@ class MyDataModule(pl.LightningDataModule):
         datasets: DictConfig,
         num_workers: DictConfig,
         batch_size: DictConfig,
+        tokenizer: str,
+        max_length: int,
     ):
         super().__init__()
         self.datasets = datasets
         self.num_workers = num_workers
         self.batch_size = batch_size
+        self.max_length = max_length
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
 
         self.train_dataset: Optional[Dataset] = None
         self.val_datasets: Optional[Sequence[Dataset]] = None
@@ -48,16 +54,41 @@ class MyDataModule(pl.LightningDataModule):
 
     def prepare_data(self) -> None:
         # download only
-        pass
+        ews_dataset = load_dataset(
+            "json",
+            data_files={
+                "train": self.datasets.train.path,
+                "validation": self.datasets.val.path,
+            },
+        )
+        self.train_dataset = ews_dataset["train"]
+        self.val_dataset = ews_dataset["validation"]
+
+    def tokenize_data(self, example):
+        return self.tokenizer(
+            example["sentence"],
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+        )
 
     def setup(self, stage: Optional[str] = None):
-        # Here you should instantiate your datasets, you may also split the train into train and validation if needed.
-        if stage is None or stage == "fit":
-            self.train_dataset = hydra.utils.instantiate(self.datasets.train)
-            self.val_datasets = [
-                hydra.utils.instantiate(dataset_cfg)
-                for dataset_cfg in self.datasets.val
-            ]
+        # Here you should instantiate your datasets, you may also
+        # split the train into train and validation if needed.
+        if stage == "fit" or stage is None:
+            self.train_dataset = self.train_dataset.map(
+                self.tokenize_data, batched=True
+            )
+            self.train_dataset.set_format(
+                type="torch", columns=["input_ids", "attention_mask", "label"]
+            )
+
+            self.val_dataset = self.val_dataset.map(self.tokenize_data, batched=True)
+            self.val_dataset.set_format(
+                type="torch",
+                columns=["input_ids", "attention_mask", "label"],
+                output_all_columns=True,
+            )
 
         if stage is None or stage == "test":
             self.test_datasets = [
@@ -74,17 +105,13 @@ class MyDataModule(pl.LightningDataModule):
             worker_init_fn=worker_init_fn,
         )
 
-    def val_dataloader(self) -> Sequence[DataLoader]:
-        return [
-            DataLoader(
-                dataset,
-                shuffle=False,
-                batch_size=self.batch_size.val,
-                num_workers=self.num_workers.val,
-                worker_init_fn=worker_init_fn,
-            )
-            for dataset in self.val_datasets
-        ]
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            shuffle=False,
+            batch_size=self.batch_size.val,
+            num_workers=self.num_workers.val,
+        )
 
     def test_dataloader(self) -> Sequence[DataLoader]:
         return [
