603a1fc5768b33cc6aae54cb70c7fbde1e39ede3
Quang-Duy Tran <55122391+Quang-Duy@users.noreply.github.com>
2021-12-21 14:40:13 -0800

    Feature: load_dataset (v.0.1.0)
    Add data module to load, preprocess and format data for training pipeline
    Unit test is WIP, with 65% coverage
    
    Co-authored-by: ktl014 <ktl014@ucsd.edu>

--- src/pl_data/datamodule.py
+++ src/pl_data/datamodule.py
@@ -1,13 +1,17 @@
 import random
 from typing import Optional, Sequence
 
+import os
 import hydra
 import numpy as np
+import pandas as pd
 import omegaconf
 import pytorch_lightning as pl
 import torch
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset, ClassLabel
+from transformers import AutoTokenizer
 
 from src.common.utils import PROJECT_ROOT
 
@@ -36,36 +40,138 @@ class MyDataModule(pl.LightningDataModule):
         datasets: DictConfig,
         num_workers: DictConfig,
         batch_size: DictConfig,
+        tokenizer: str,
+        max_length: int,
     ):
+        """Initialize data variables from .env file
+
+        Usage:
+        >>> from src.pl_data.datamodule import MyDataModule
+        >>> my_dataset = MyDataModule()
+
+        Args:
+            datasets (DictConfig): dataset contains train, val and test
+            num_workers (DictConfig): number of thread/processes working parallel
+            batch_size (DictConfig): batch size for train, val and test
+            tokenizer (str): model's name
+            max_length (int): max number of word per line (BERT can store max 512 words per line)
+        """
         super().__init__()
         self.datasets = datasets
         self.num_workers = num_workers
         self.batch_size = batch_size
+        self.max_length = max_length
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
 
         self.train_dataset: Optional[Dataset] = None
         self.val_datasets: Optional[Sequence[Dataset]] = None
         self.test_datasets: Optional[Sequence[Dataset]] = None
 
     def prepare_data(self) -> None:
-        # download only
-        pass
+        """Load dataset and split 80-train and 20-validation
+
+        Usage:
+        >>> from src.pl_data.datamodule import MyDataModule
+        >>> my_dataset = MyDataModule()
+        >>> my_dataset.prepare_data()
+
+        Returns:
+
+        """
+        # Split train to first 80%
+        self.train_dataset = load_dataset(
+            "csv",
+            data_files={
+                "train": self.datasets.train.path,
+            },
+            split='train[:80%]'
+        )
+
+        # Split val to last 20%
+        self.val_dataset = load_dataset(
+            'csv',
+            data_files={
+                'train': self.datasets.train.path
+            },
+            split='train[-20%:]')
+        # print(f"train size: {len(self.train_dataset)} --- val size: {len(self.val_dataset)}")
+
+        # Save all unique labels
+        dset_df = pd.read_csv(self.datasets.train.path)
+        unique_labels = list(dset_df['discourse_type'].unique())
+        self.labels = ClassLabel(names=unique_labels)
+
+    def tokenize_and_label_encoding(self, example):
+        """ Tokenize features and encode label
+
+        Usage:
+        >>> from src.pl_data.datamodule import MyDataModule
+        >>> my_dataset = MyDataModule()
+        >>> my_dataset.tokenize_and_label_encoding(my_dataset.train_dataset)
+
+        Args:
+            example (datasets.Dataset): dataset
+
+        Returns:
+            tokens (transformers.BatchEncoding): tokenized and label-encoded dataset
+        """
+        tokens = self.tokenizer(
+            example["discourse_text"],
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+        )
+        tokens['discourse_type'] = self.labels.str2int(example['discourse_type'])
+        return tokens
 
     def setup(self, stage: Optional[str] = None):
-        # Here you should instantiate your datasets, you may also split the train into train and validation if needed.
-        if stage is None or stage == "fit":
-            self.train_dataset = hydra.utils.instantiate(self.datasets.train)
-            self.val_datasets = [
-                hydra.utils.instantiate(dataset_cfg)
-                for dataset_cfg in self.datasets.val
-            ]
+        """Perform tokenizing, encoding and formatting dataset
 
-        if stage is None or stage == "test":
-            self.test_datasets = [
-                hydra.utils.instantiate(dataset_cfg)
-                for dataset_cfg in self.datasets.test
-            ]
+        Usage:
+        >>> from src.pl_data.datamodule import MyDataModule
+        >>> my_dataset = MyDataModule()
+        >>> my_dataset.setup()
+
+        Args:
+            stage (str): stage can either be fit, test, or None
+
+        Returns:
+
+        """
+        # Here you should instantiate your datasets, you may also
+        if stage == "fit" or stage is None:
+            self.train_dataset = self.train_dataset.map(
+                self.tokenize_and_label_encoding, batched=True
+            )
+            self.train_dataset.set_format(
+                type="torch", columns=["input_ids", "attention_mask", "discourse_type"]
+            )
+
+            self.val_dataset = self.val_dataset.map(
+                self.tokenize_and_label_encoding, batched=True
+            )
+            self.val_dataset.set_format(
+                type="torch",
+                columns=["input_ids", "attention_mask", "discourse_type"]
+            )
+
+        # if stage is None or stage == "test":
+        #     self.test_datasets = [
+        #         hydra.utils.instantiate(dataset_cfg)
+        #         for dataset_cfg in self.datasets.test
+        #     ]
 
     def train_dataloader(self) -> DataLoader:
+        """Apply DataLoader on train dataset - shuffle, batch_size, and workers
+
+        Usage:
+        >>> from src.pl_data.datamodule import MyDataModule
+        >>> my_dataset = MyDataModule()
+        >>> my_dataset.train_dataloader()()
+
+        Returns:
+            Sequence[DataLoader]: return a Sequence of size = len(train_dataset) / batch_size
+        """
         return DataLoader(
             self.train_dataset,
             shuffle=True,
@@ -74,19 +180,35 @@ class MyDataModule(pl.LightningDataModule):
             worker_init_fn=worker_init_fn,
         )
 
-    def val_dataloader(self) -> Sequence[DataLoader]:
-        return [
-            DataLoader(
-                dataset,
-                shuffle=False,
-                batch_size=self.batch_size.val,
-                num_workers=self.num_workers.val,
-                worker_init_fn=worker_init_fn,
-            )
-            for dataset in self.val_datasets
-        ]
+    def val_dataloader(self) -> DataLoader:
+        """Apply DataLoader on val dataset - shuffle, batch_size, and workers
+
+        Usage:
+        >>> from src.pl_data.datamodule import MyDataModule
+        >>> my_dataset = MyDataModule()
+        >>> my_dataset.val_dataloader()
+
+        Returns:
+            Sequence[DataLoader]: return a Sequence of size = len(val_dataset) / batch_size
+        """
+        return DataLoader(
+            self.val_dataset,
+            shuffle=False,
+            batch_size=self.batch_size.val,
+            num_workers=self.num_workers.val,
+        )
 
     def test_dataloader(self) -> Sequence[DataLoader]:
+        """Apply DataLoader on val dataset - shuffle, batch_size, and workers
+
+        Usage:
+        >>> from src.pl_data.datamodule import MyDataModule
+        >>> my_dataset = MyDataModule()
+        >>> my_dataset.test_dataloader()
+
+        Returns:
+            Sequence[DataLoader]: return a Sequence of size = len(test_dataset) / batch_size
+        """
         return [
             DataLoader(
                 dataset,
@@ -112,6 +234,10 @@ def main(cfg: omegaconf.DictConfig):
     datamodule: pl.LightningDataModule = hydra.utils.instantiate(
         cfg.data.datamodule, _recursive_=False
     )
+
+    datamodule.prepare_data()
+    datamodule.setup()
+    print(next(iter(datamodule.train_dataloader()))["input_ids"].shape)
 
 
 if __name__ == "__main__":
