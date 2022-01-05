21a5509800c6af449722dbae6f3e11b239096eda
Quang-Duy Tran <quangduytran99@gmail.com>
2022-01-04 22:30:24 -0800

    Update (lambda_handler.py): Added sample call

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -8,8 +8,22 @@ inferencing_instance = EWSONNXPredictor("./models/model.onnx")
 
 
 def lambda_handler(event, context):
-	"""
-	Lambda function handler for predicting linguistic acceptability of the given sentence
-	"""
-	print(event)
-	return inferencing_instance.predict(event["sentence"])
+    """Lambda function handler for predicting linguistic acceptability of
+	the given sentence
+
+    Args:
+        event:
+        context:
+
+    Returns:
+
+    """
+    print(event)
+    return inferencing_instance.predict(event["sentence"])
+
+
+if __name__ == "__main__":
+    sentence = "In this situation they need someone to guid them " \
+               "through the online course, or they can take a home tutour."
+    test = {"sentence": sentence}
+    lambda_handler(test, None)
