19480b793130bdd58995526936ab98ca93b41ddd
Quang-Duy Tran <quangduytran99@gmail.com>
2022-01-05 23:03:08 -0800

    Update (src/run.py & common/utils.py): moved method log_hyperparameters from utils.py to run.py, and remove import pytorch_lightning Update (Dockerfile): add environment variables for wandb

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM amazon/aws-lambda-python
+FROM public.ecr.aws/lambda/python:3.8
 
 ARG AWS_ACCESS_KEY_ID
 ARG AWS_SECRET_ACCESS_KEY
 ARG MODEL_DIR=./models
 ARG WANDB_DIR=./wandb_dir
+ARG WANDB_CONFIG_DIR=./wandb_config_dir
+ARG WANDB_CACHE_DIR=./wandb_cache_dir
 RUN mkdir $MODEL_DIR
 RUN mkdir $WANDB_DIR
+RUN mkdir $WANDB_CONFIG_DIR
+RUN mkdir $WANDB_CACHE_DIR
+
 
 ENV TRANSFORMERS_CACHE=$MODEL_DIR \
     TRANSFORMERS_VERBOSITY=error
@@ -23,6 +28,8 @@ RUN pip install -r requirements.txt --no-cache-dir
 COPY ./ ./
 ENV PROJECT_ROOT=./
 ENV WANDB_DIR=$WANDB_DIR
+ENV WANDB_CONFIG_DIR=$WANDB_CONFIG_DIR
+ENV WANDB_CACHE_DIR=$WANDB_CACHE_DIR
 
 ENV PYTHONPATH "${PYTHONPATH}:./"
 ENV LC_ALL=C.UTF-8
@@ -31,8 +38,12 @@ ENV LANG=C.UTF-8
 # pulling the trained model
 RUN dvc pull models/model.onnx.dvc
 
-RUN ls
 RUN chmod -R 0755 $WANDB_DIR
+RUN chmod -R 0755 $WANDB_CONFIG_DIR
+RUN chmod -R 0755 $WANDB_CACHE_DIR
+
+RUN chmod -R 0755 ./
+
 RUN python -m src.lambda_handler
 RUN chmod -R 0755 $MODEL_DIR
 CMD ["src.lambda_handler.lambda_handler"]
--- src/common/utils.py
+++ src/common/utils.py
@@ -6,8 +6,6 @@ from pathlib import Path
 from typing import Optional
 
 import dotenv
-import pytorch_lightning as pl
-from omegaconf import DictConfig, OmegaConf
 
 
 def get_env(env_name: str, default: Optional[str] = None) -> str:
@@ -49,47 +47,6 @@ def load_envs(env_file: Optional[str] = None) -> None:
         If None it searches for a `.env` file in the project.
     """
     dotenv.load_dotenv(dotenv_path=env_file, override=True)
-
-
-STATS_KEY: str = "stats"
-
-
-# Adapted from https://github.com/hobogalaxy/lightning-hydra-template/blob
-# /6bf03035107e12568e3e576e82f83da0f91d6a11/src/utils/template_utils.py#L125
-def log_hyperparameters(
-    cfg: DictConfig,
-    model: pl.LightningModule,
-    trainer: pl.Trainer,
-) -> None:
-    """This method controls which parameters from Hydra
-        config are saved by Lightning loggers.
-    Additionally saves:
-        - sizes of train, val, test dataset
-        - number of trainable model parameters
-    Args:
-        cfg (DictConfig): [description]
-        model (pl.LightningModule): [description]
-        trainer (pl.Trainer): [description]
-    """
-    hparams = OmegaConf.to_container(cfg, resolve=True)
-
-    # save number of model parameters
-    hparams[f"{STATS_KEY}/params_total"] = sum(p.numel()
-                                               for p in model.parameters())
-    hparams[f"{STATS_KEY}/params_trainable"] = sum(
-        p.numel() for p in model.parameters() if p.requires_grad
-    )
-    hparams[f"{STATS_KEY}/params_not_trainable"] = sum(
-        p.numel() for p in model.parameters() if not p.requires_grad
-    )
-
-    # send hparams to all loggers
-    trainer.logger.log_hyperparams(hparams)
-
-    # disable logging any more hyperparameters for all loggers
-    # (this is just a trick to prevent trainer from logging hparams of model,
-    # since we already did that above)
-    trainer.logger.log_hyperparams = lambda params: None
 
 
 # Load environment variables
