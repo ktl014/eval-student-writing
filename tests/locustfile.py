d80a5705703decf0179c730d9d9578a894c09484
Quang-Duy Tran <quangduytran99@gmail.com>
2022-01-07 00:16:06 -0800

    Add (locustfile.py): Add load test script to stress test server on AWS Lambda through API Gateway

---
+++ tests/locustfile.py
@@ -0,0 +1,19 @@
+from locust import HttpUser, task, between
+import json
+
+# Loading the test JSON data
+with open('test_sample.json') as f:
+    test_data = json.loads(f.read())
+
+# Creating an API User class inheriting from Locust's HttpUser class
+class APIUser(HttpUser):
+
+    # Setting the host name and wait_time
+    host = 'https://yvvkq85w94.execute-api.us-west-1.amazonaws.com'
+    wait_time = between(1, 5)
+
+    # Defining the post task using the JSON test data
+    @task()
+    def predict_endpoint(self):
+        self.client.post('/Deploy/predict', json=test_data)
+
